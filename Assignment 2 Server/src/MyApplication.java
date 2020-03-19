ea525be789a0d2b514c9e27abf176685095a7b43
Albert-Andrei <moldovanualiberta@gmial.com>
2020-03-19 18:00:38 +0100

    acu e bine

--- Assignment 2 Server/src/MyApplication.java
+++ Assignment 2 Server/src/MyApplication.java
@@ -2,16 +2,23 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import model.Model;
 import model.ModelManager;
+import server.LogInClientConnector;
 import view.ViewHandler;
 import viewModel.ViewModelFactory;
 
+import java.io.IOException;
+
 public class MyApplication extends Application
 {
-  public void start(Stage primaryStage)
+  public void start(Stage primaryStage) throws IOException
   {
     Model model = new ModelManager();
     ViewModelFactory viewModelFactory = new ViewModelFactory(model);
     ViewHandler view = new ViewHandler(viewModelFactory);
     view.start(primaryStage);
+    LogInClientConnector server = new LogInClientConnector(model);
+    Thread t = new Thread(server);
+    t.start();
+
   }
 }
