f4b3b4aa2d2645044a2efa1f56cd8b07ac688350
Ion Creciun <ion.creciun1@gmail.com>
2020-03-19 17:16:22 +0100

    login windows finished

--- Assignment 2 Client/src/model/ModelManager.java
+++ Assignment 2 Client/src/model/ModelManager.java
@@ -3,6 +3,8 @@ package model;
 import mediator.LoginClient;
 import mediator.ServerModel;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 
 public class ModelManager implements Model
@@ -10,11 +12,13 @@ public class ModelManager implements Model
   private ServerModel serverModel;
   public ModelManager()
   {
-
   }
+
   @Override public boolean verifyLog(String password) throws IOException
   {
     this.serverModel = new LoginClient();
+    serverModel.connect();
 return serverModel.verifyLog(password);
   }
+
 }
--- Assignment 2 Client/src/view/LoginViewController.java
+++ Assignment 2 Client/src/view/LoginViewController.java
@@ -1,10 +1,19 @@
 package view;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import viewModel.LoginViewModel;
 
+import java.io.IOException;
+
 public class LoginViewController
 {
+  public TextField username;
+  public PasswordField password;
+  public Label error;
   private ViewHandler viewHandler;
   private LoginViewModel viewModel;
   private Region root;
@@ -22,9 +31,31 @@ public class LoginViewController
     this.viewHandler = viewHandler;
     this.viewModel = viewModel;
     this.root = root;
+    password.textProperty().bindBidirectional(viewModel.passwordProperty());
+
   }
-  public void openChat()
+  public void openChat() throws IOException
   {
-    viewHandler.openView("chat");
+
+
+    if(password.getText().equals(""))
+    {
+      error.setText("Insert Password");
+    }
+    if(viewModel.verifyPass())
+      viewHandler.openView("chat");
+    }
+
+  public void verify(KeyEvent keyEvent) throws IOException
+  {
+
+    System.out.println(viewModel.verifyPass());
+    if(!viewModel.verifyPass())
+    {
+      error.setText("Wrong password");
+    }
+    else {
+      error.setText("Oki Talky");
+    }
   }
 }
